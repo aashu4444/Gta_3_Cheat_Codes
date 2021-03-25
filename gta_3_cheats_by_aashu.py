57ef81aef70f4a5f660d73f72cd4bb1cd5108192
Aashu <prajapatiaashu4444@gmail.com>
2021-03-25 10:20:11 +0530

    My Python

---
+++ gta_3_cheats_by_aashu.py
@@ -0,0 +1,96 @@
+try:
+    import pyautogui
+    import keyboard
+
+    while True:
+        if keyboard.is_pressed("shift + q"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("bangbangbang")
+            
+        elif keyboard.is_pressed("shift + w"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("boooooring")
+
+        elif keyboard.is_pressed("shift + e"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("chittychittybb")
+
+        elif keyboard.is_pressed("shift + r"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("anicesetofwheels")
+
+        elif keyboard.is_pressed("shift + t"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("cornerslikemad")
+
+        elif keyboard.is_pressed("shift + y"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("gesundheit")
+
+        elif keyboard.is_pressed("shift + u"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("giveusatank")
+
+        elif keyboard.is_pressed("shift + i"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("gunsgunsguns")
+
+        elif keyboard.is_pressed("shift + o"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("ifiwerearichman")
+
+        elif keyboard.is_pressed("shift + p"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("ifiwerearichman")
+
+        elif keyboard.is_pressed("shift + a"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("ilikedressingup")
+
+        elif keyboard.is_pressed("shift + s"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("ilikescotland")
+
+        elif keyboard.is_pressed("shift + d"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("itsallgoingmaaad")
+
+        elif keyboard.is_pressed("shift + f"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("madweather")
+
+        elif keyboard.is_pressed("shift + g"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("morepoliceplease")
+
+        elif keyboard.is_pressed("shift + h"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("nastylimbscheat")
+
+        elif keyboard.is_pressed("shift + j"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("nobodylikesme")
+
+        elif keyboard.is_pressed("shift + k"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("nopoliceplease")
+
+        elif keyboard.is_pressed("shift + l"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("peasoup")
+
+        elif keyboard.is_pressed("shift + z"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("skincancerforme")
+
+        elif keyboard.is_pressed("shift + x"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("timeflieswhenyou")
+
+        elif keyboard.is_pressed("shift + c"):
+            pyautogui.keyDown("backspace")
+            pyautogui.write("weaponsforall")
+
+except Exception as error:
+    print(error)
+    input()
